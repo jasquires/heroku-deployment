188756330952abd2ebc5ef221ae8f5e41edfba90
Aubrey Squires <aubreysquires@Aubreys-MacBook-Pro.local>
2017-10-20 15:30:07 -0500

    Implemented basic site

---
+++ client/app.js
@@ -0,0 +1,15 @@
+angular.module('heroku', [
+    'ngRoute',
+    'heroku.controllers'
+])
+
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '/views/home.html',
+            controller: 'HomeController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
+}])
